8588705f96557902a3630ccecb2c39bf0a74f5ff
Sergio Gonzalez Garrido <sergio.gonzalez.spain@gmail.com>
2023-05-26 21:48:10 +0200

    Peticiones de sugerencias

--- src/app/api/suggestions/route.ts
+++ src/app/api/suggestions/route.ts
@@ -1,34 +1,14 @@
+import { GoogleService } from '@/service/google/google.service';
 import { NextResponse } from 'next/server';
-
-const API =
-	'https://suggestqueries.google.com/complete/search?client=chrome&q=';
-
-function getSuggestionFormat(term: string, suggestion: string) {
-	let length = term.length;
-	let text = suggestion.substring(length);
-	return text.padStart(suggestion.length, ' ');
-}
-
-async function getSuggestions(term: string) {
-	const response = await fetch(`${API}${term}`);
-	const data = await response.json();
-	const suggestions = data[1][0];
-
-	return {
-		format: getSuggestionFormat(term, suggestions),
-		suggestions,
-	};
-}
 
 export async function GET(request: Request) {
 	const { searchParams } = new URL(request.url);
 	const term = searchParams.get('term');
 
-	if (term === null) {
-		return new Response('Invalid ID', { status: 400 });
-	}
+	if (!term) return new Response('Invalid ID', { status: 400 });
 
-	const suggestions = await getSuggestions(term);
+	const googleService = new GoogleService();
+	const suggestions = await googleService.getSuggestions(term);
 
 	return NextResponse.json(suggestions);
 }
