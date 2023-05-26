ecfbed8fbd9f2a454d3e00327c9296e0cf326735
Sergio Gonzalez Garrido <sergio.gonzalez.spain@gmail.com>
2023-05-26 21:49:38 +0200

    Cabeceras personalizadas

---
+++ src/components/heading/index.tsx
@@ -0,0 +1,23 @@
+export function H1({ children, className }: any) {
+	return <h1 className={`text-5xl ${className}`}>{children}</h1>;
+}
+
+export function H2({ children, className }: any) {
+	return <h2 className={`text-4xl ${className}`}>{children}</h2>;
+}
+
+export function H3({ children, className }: any) {
+	return <h3 className={`text-3xl ${className}`}>{children}</h3>;
+}
+
+export function H4({ children, className }: any) {
+	return <h4 className={`text-2xl ${className}`}>{children}</h4>;
+}
+
+export function H5({ children, className }: any) {
+	return <h5 className={`text-xl ${className}`}>{children}</h5>;
+}
+
+export function H6({ children, className }: any) {
+	return <h6 className={`text-lg ${className}`}>{children}</h6>;
+}
