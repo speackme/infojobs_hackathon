7221e600b5a782a208037424342ce8b714c56fc3
Sergio Gonzalez Garrido <sergio.gonzalez.spain@gmail.com>
2023-05-29 21:54:36 +0200

    Heading responsive

--- src/components/heading/index.tsx
+++ src/components/heading/index.tsx
@@ -1,21 +1,21 @@
 export function H1({ children, className }: any) {
-	return <h1 className={`text-5xl ${className}`}>{children}</h1>;
+	return <h1 className={`text-3xl md:text-5xl ${className}`}>{children}</h1>;
 }
 
 export function H2({ children, className }: any) {
-	return <h2 className={`text-4xl ${className}`}>{children}</h2>;
+	return <h2 className={`text-2xl md:text-4xl ${className}`}>{children}</h2>;
 }
 
 export function H3({ children, className }: any) {
-	return <h3 className={`text-3xl ${className}`}>{children}</h3>;
+	return <h3 className={`text-xl md:text-3xl ${className}`}>{children}</h3>;
 }
 
 export function H4({ children, className }: any) {
-	return <h4 className={`text-2xl ${className}`}>{children}</h4>;
+	return <h4 className={`text-lg md:text-2xl ${className}`}>{children}</h4>;
 }
 
 export function H5({ children, className }: any) {
-	return <h5 className={`text-xl ${className}`}>{children}</h5>;
+	return <h5 className={`text-lg md:text-xl ${className}`}>{children}</h5>;
 }
 
 export function H6({ children, className }: any) {
